64a9c4f1e8f9c9e64348f4c70b86d3bf42889989
Leo Lin <leo.lin@snackpass.co>
2025-03-22 16:26:02 -0700

    [feat]: write notes to file

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,27 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn update_file(path: &str, blob: &str) -> Result<(), String> {
+    // TODO: Make base path configurable
+    let base_path = PathBuf::from("./target/debug/");
+    let file_path = base_path.join(path.trim_start_matches('/')); // Remove leading slash if present
+    
+    let mut file = File::create(&file_path)
+        .map_err(|e| format!("Failed to create file: {}", e))?;
+    
+    file.write_all(blob.as_bytes())
+        .map_err(|e| format!("Failed to write to file: {}", e))?;
+    
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![update_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
